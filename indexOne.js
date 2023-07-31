0c5ebcbbcc3a757309f0931cf38be1e1ceff4cc9
Shiham123 <s36651786@gmail.com>
2023-07-31 10:38:43 +0600

    learning nodeJS

--- indexOne.js
+++ indexOne.js
@@ -1,2 +1,14 @@
-const text = 'this is text';
-console.log(text);
+const fileSystem = require('fs');
+
+fileSystem.writeFile(
+  'demoOne.txt',
+  'this is file system modified',
+  function (error) {
+    if (error) {
+      console.log('failed written');
+      console.log(error);
+    } else {
+      console.log('written is successful');
+    }
+  }
+);
